17af7ad370a36d8b6fa63ff80a38dc5d53d1e168
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-08-31 16:03:29 +0000

    disabled js-lint

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Server Status: {{status}}</h1>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                status: 'Critical'
+            }
+        }
+    
+    }
+</script>
+
+<style>
+
+</style>
